3b415176295f27af8ba3b26ed403a46957d22a2b
Daniel Munger and Maxwell Cady <daniel@>
2016-11-01 13:54:10 -0700

    add deposit and withdrawal functions.

--- js/scripts.js
+++ js/scripts.js
@@ -5,39 +5,69 @@ var Account = function(firstName, lastName, balance) {
 }
 
 Account.prototype.deposit = function(depositAmount){
-  return this.balance + depositAmount;
+  this.balance += depositAmount;
+  return this.balance;
+}
+Account.prototype.withdrawal = function(withdrawalAmount){
+  this.balance -= withdrawalAmount;
+  return this.balance;
 }
 
 $(function() {
-var inputtedFirstName;
-var inputtedLastName;
-var initialDeposit;
-var depositAmount;
+  var inputtedFirstName;
+  var inputtedLastName;
+  var initialDeposit;
+  var depositAmount;
+  var depositFloat;
+  var withdrawalAmount;
+  var withdrawalFloat;
+  var newAccount = new Account();
+
+  $("#retCust").click(function() {
+    $("#depWith").show();
+    $("#getAccount").hide();
+    $("#initialDeposit").hide();
+    newAccount.balance = 0;
+  });
 
   $("#getAccount").submit(function() {
     event.preventDefault();
     inputtedFirstName = $("#firstName").val();
     inputtedLastName = $("#lastName").val();
     $("#deposit").fadeIn();
+    newAccount.balance = 0;
   });
 
   $("#initialDeposit").submit(function(event) {
     event.preventDefault();
-    initialDeposit = $("#initialAmount").val();
-    var newAccount = new Account(inputtedFirstName, inputtedLastName, initialDeposit);
-    console.log(newAccount);
+    initialDeposit = parseFloat($("#initialAmount").val());
+    newAccount.firstName = inputtedFirstName;
+    newAccount.lastName = inputtedLastName;
+    newAccount.balance += initialDeposit;
+
+    $("#depWith").fadeIn();
+    $(".balance").fadeIn();
+    $("#outputtedBalance").html("Hello, " + newAccount.firstName + "! Your balance is $" + newAccount.balance);
   });
-  $("#retCust").click(function() {
-    
-    $("#depWith").show();
-    $("#getAccount").hide();
-  });
+
   $("#depositForm").submit(function(event) {
     event.preventDefault();
-    depositAmount = $("#makeDeposit").val();
+    depositFloat = parseFloat($("#makeDeposit").val());
+    depositAmount = parseFloat(depositFloat.toFixed(2));
+    newAccount.balance = newAccount.deposit(depositAmount);
+    $("#outputtedBalance").html("Hello, " + newAccount.firstName + "! Your new balance is $" + newAccount.balance);
+    $(".balance").fadeIn();
+  });
 
-    console.log(newAccount.deposit(depositAmount));
-
+  $("#withdrawalForm").submit(function(event) {
+    event.preventDefault();
+    withdrawalFloat = parseFloat($("#makeWithdrawal").val());
+    withdrawalAmount = parseFloat(withdrawalFloat.toFixed(2));
+    newAccount.balance = newAccount.withdrawal(withdrawalAmount);
+    console.log(newAccount.balance);
+    $("#outputtedBalance").text(newAccount.balance);
+    $("#outputtedBalance").html("Hello, " + newAccount.firstName + "! Your new balance is $" + newAccount.balance);
+    $(".balance").fadeIn();
   });
 
 })
